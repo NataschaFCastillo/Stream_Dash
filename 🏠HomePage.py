cd8e740c6cec1ba1bf36ea3cdc838d9a5d258f02
NataschaFCastillo <163320388+NataschaFCastillo@users.noreply.github.com>
2024-11-22 18:43:52 +0000

    Add files via upload

---
+++ 🏠HomePage.py
@@ -0,0 +1,6 @@
+# Import Libraries
+import streamlit as st
+
+st.title("Hey There! Are you excited to discover some fantastic tips for you dining  al fresco? Let's dive in!")
+st.header("Welcome to Restaurant Recomendation Sytem ")
+st.write("Discover a treasure of top-notch advice waiting just for you.")
