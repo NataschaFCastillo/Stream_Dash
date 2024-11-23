5f25fff20a79b1a2b4c627146bd80ba3a0dad681
NataschaFCastillo <163320388+NataschaFCastillo@users.noreply.github.com>
2024-11-23 20:35:27 +0000

    Update 03. Feedback📝.py

--- pages/03. Feedback📝.py
+++ pages/03. Feedback📝.py
@@ -28,7 +28,21 @@ def save_feedback(folder_path, name, feedback):
         df.to_csv(file_path, index=False)  # Create the new file and save data
 
 
+
 # Get inputs from the user
+st.write("Did you enjoy your recommendation?")
+yes_button_clicked = st.button("Yes!")
+no_button_clicked = st.button("No!")
+
+if yes_button_clicked:
+    st.write("Glad you enjoyed it!?")
+elif no_button_clicked:
+    st.write("We're sorry to hear that. We'll strive to improve.")
+
+experiene_rating = st.slider("On a scale of 1 to 10, how would you rate your experience today?", 1, 10, 5)
+
+# Get inputs from the user
+st.write("We value your feedback. Please tell us more about your experience?")
 name = st.text_input('Enter your name:')
 feedback = st.text_area('Enter your feedback:')
 
