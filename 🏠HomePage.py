d7dab7bd3392ba939bb7d50d2474c86dd87b83d7
NataschaFCastillo <163320388+NataschaFCastillo@users.noreply.github.com>
2024-11-23 22:45:41 +0000

    Update 🏠HomePage.py

--- 🏠HomePage.py
+++ 🏠HomePage.py
@@ -2,5 +2,6 @@
 import streamlit as st
 
 st.title("Hey There! Are you excited to discover some fantastic tips for you dining  al fresco? Let's dive in!")
-st.header("Welcome to Restaurant Recomendation Sytem ")
+#st.header("Welcome to Restaurant Recomendation Sytem ")
+
 st.write("Discover a treasure of top-notch advice waiting just for you.")
