de341a99f8b930a5e877fff260e7d10c1164fbbe
NataschaFCastillo <163320388+NataschaFCastillo@users.noreply.github.com>
2024-11-24 07:40:09 +0000

    Update 00. PreProcessing.py

--- pages/00. PreProcessing.py
+++ pages/00. PreProcessing.py
@@ -4,21 +4,29 @@ import plotly.express as px
 
 # File_path
 file_path = "Restaurant_Ratings.xlsx"
-st.title("PreProcessing")
+
+# Title with Icon and Custom Font Size
+st.markdown("<h1 style='font-size:36px;'>⏳ PreProcessing</h1>", unsafe_allow_html=True)
+# Description with Slightly Smaller Font and Custom Styling
+st.markdown("<p style='font-size:18px;'>A summary of the steps made during the preprocessing step</p>", unsafe_allow_html=True)
+
 
 ratings_df = pd.read_excel(file_path, sheet_name=0)
 details_df = pd.read_excel(file_path, sheet_name=1)
 
 # Display columns for ratings_df
-st.header("Columns in Restaurant Ratings")
-st.write("**Columns:**", ratings_df.columns.tolist())  # Display columns
+#st.header("Sheet Restaurant Ratings")
+st.markdown("## Table Restaurant Ratings")
+st.write("**We have 4 columns below:**", ratings_df.columns.tolist())  # Display columns
 
 # Display columns for details_df
-st.header("Columns in Restaurant Details")
-st.write("**Columns:**", details_df.columns.tolist())  # Display columns
+st.header("Table Restaurant Details")
+st.write("**We have 5 Columns below:**", details_df.columns.tolist())  # Display columns
 
-st.write("Merging two tables based on Restaurant_ID")
-st.write("Dropping column Name of Restaurant Details as we already have Restaurant_Name in Restaurant Ratings")
+#st.title("Steps done in PreProcessing")
+st.markdown("### Steps done in PreProcessing")
+st.write("1. Merging two tables based on Restaurant_ID")
+st.write("2. Dropping column Name of Restaurant Details as we already have Restaurant_Name in Restaurant Ratings")
 
 merged_df = pd.merge(ratings_df, details_df, on="Restaurant_ID", how="left")
 merged_df.drop(columns=["Name"], inplace=True)
@@ -43,15 +51,15 @@ merged_df['Price'].fillna("None", inplace=True)
 st.write("""
 ### Transformation Applied:
 Replaced the values in the `Overall_Rating` column as follows:
-- 0 → "Don't Like"
-- 1 → "Okay"
-- 2 → "I like it"
+- 0 → "Don't Like it"
+- 1 → "I like it"
+- 2 → "I love it"
 """)
 
 merged_df['Overall_Rating'].replace({
-    0: "Don't Like",
-    1: "Okay",
-    2: "I like it"
+    0: "Don't Like it",
+    1: "I like it",
+    2: "I love it"
 }, inplace=True)
 
 
